2a26c83a7efea8ef60a118edefabf648f941b0e4
Mrkhun99 <160562780+Mrkhun99@users.noreply.github.com>
2024-11-04 14:53:13 +0700

    Update Ex1-hobbie.dart

--- W4-s1/Ex1-hobbie.dart
+++ W4-s1/Ex1-hobbie.dart
@@ -1,76 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('My Hobbies'),
-      ),
-      backgroundColor: Colors.grey[50],
-      body: Padding(padding: EdgeInsets.all(40),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            
-            decoration: BoxDecoration(
-              color: Colors.green,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: Center(
-              child: Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.travel_explore),
-                  ),
-                  Text('Travelling'),
-                ],
-              ),
-            ),
-          ),
+  runApp(Myhobbie());
+}
 
-          Container(
-           margin: EdgeInsets.fromLTRB(0,10,0,0),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20),
+class Myhobbie extends StatefulWidget{
+  @override
+  State<Myhobbie> createState() => _MyhobbieState();
+}
+
+class _MyhobbieState extends State<Myhobbie> {
+  @override
+
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[50],
+        appBar: AppBar(
+          title: Text("My Hobbies"),
+          backgroundColor: Colors.purple[50],
+             
+        ),
+        body: Padding(padding: const EdgeInsets.all(40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            HobbyCard(
+              icon:Icons.travel_explore,
+              text: ("Travelling"),
+              color: Colors.green,
             ),
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: Center(
-              child: Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.skateboarding),
-                  ),
-                  Text('Skating'),
-                ],
-              ),
+            
+            SizedBox(height: 10,),
+
+            HobbyCard(
+              icon:Icons.skateboarding,
+              text:("Skating"),
+              color:Colors.grey,
             ),
-          ),
-          
-          Container(
-           margin: EdgeInsets.fromLTRB(0,10,0,40),
-            decoration: BoxDecoration(
+          ],
+        ),
+        ),
+      ),
+    );
+  }
+}
+
+class HobbyCard extends StatelessWidget{
+  
+  final IconData icon;
+  final String text;
+  final Color color;
+
+  const HobbyCard({
+    required this.icon,
+    required this.text,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+      decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: [
+          Icon(icon,color: Colors.white, size: 30),
+          SizedBox(width: 20),
+          Text(
+            text, style: TextStyle(
               color: Colors.red,
-              borderRadius: BorderRadius.circular(20),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: Center(
-              child: Row(
-                children: [
-                  Padding(padding: EdgeInsets.only(right: 20),
-                  child: Icon(Icons.hiking),
-                  ),
-                  Text('Hiking'),
-                ],
-              ),
-            ),
-          ),
-          
+          )
         ],
       ),
-      ),
-    ),
-  ));
+    );
+  }
 }
+
